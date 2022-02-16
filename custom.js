e480526cd2a2a85a0584962d7e80590f68fbc5a0
Evaldo Maciel <evaldomaciel17@gmail.com>
2022-02-16 08:48:06 -0300

    :rocket: atualização de links

--- custom.js
+++ custom.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
 	console.trace = null;
 	console.clear();
+
+	const linkFacebook = "https://www.facebook.com/onerdalpha/";
+	const linkInstagram = "https://www.instagram.com/oNerdAlphaReal/";
+	const linkLinkedin = "https://www.linkedin.com/company/nerdalpha/";
+	const linkTwitter = "https://twitter.com/onerdalpha";
+
+	$(".fa-facebook-f").parent().attr("href", linkFacebook);
+	$(".fa-linkedin-in").parent().attr("href", linkLinkedin);
+	$(".fab fa-twitter").parent().attr("href", linkTwitter);
+	$(".fa-instagram").parent().attr("href", linkInstagram);
+
 });
+
+
+/** 
+ *   <div id="ajaxContent"></div>
+  <script>
+  var Webflow = Webflow || [];
+  Webflow.push(function() {
+	$.get('https://cdn.rawgit.com/alvarotrigo/fullPage.js/master/examples/simple.html', function(data) {
+	  $('#ajaxContent').append(data);
+	});
+  });
+  </script>
+
+
+  Change URL from raw.github.com or raw.githubusercontent.com to cdn.rawgit.com
+
+ */
